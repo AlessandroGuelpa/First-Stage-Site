59d95085badb4cbb806c6519f8a6c986b94a8268
AlessandroGuelpa <Alexander.guelpa@gmail.com>
2021-11-23 12:16:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -43,11 +43,11 @@ var array = [];
 // ciclo da 0 a 9
 for (var i = 0; i < 10; i++) {
   // spingi nell'array il valore della classe "item"
-  array.push(document.getElementsByClassName("item")[i]);
+  array.push(document.getElementsByClassName("item")[i].getAttribute("value"));
   // se il valore nella posizione "i" nell'array è minore di 10
   if (array[i] < 10) {
     // rendi visibile l'elemento "new"
-    document.getElementsByClassName("new")[i].setAttribute("style","visibility = visible");
+    document.getElementByClass("new")[i].setAttribute("style","visibility = visible");
     // altrimenti se il valore è maggiore/uguale a 10
   } else {
     // rendi visibile l'elemento "sold"
