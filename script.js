ca9cf9f1e38b7e5ab8c6d8c3a9c6cf9c94ddeff4
AlessandroGuelpa <67553276+AlessandroGuelpa@users.noreply.github.com>
2021-11-23 11:43:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ var array = [];
 // ciclo da 0 a 9
 for (var i = 0; i < 10; i++) {
   // spingi nell'array il valore della classe "item"
-  array.push(document.getElementsByClassName("item")[i].getAttribute("value"));
+  array.push(document.getElementsByClassName("item")[i]);
   // se il valore nella posizione "i" nell'array è minore di 10
   if (array[i] < 10) {
     // rendi visibile l'elemento "new"
