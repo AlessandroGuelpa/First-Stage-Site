4508cff3b3c0c79813823b8cbbda82405387b135
AlessandroGuelpa <Alexander.guelpa@gmail.com>
2021-11-23 15:37:02 +0100

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,17 +1,12 @@
-var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
+  var slideIndex = 0;
   var slide = document.getElementsByClassName("slide");
-  var bullet = document.getElementsByClassName("bullet");
-  for (i = 0; i < slide.length; i++) {
-    slide[i].style.display = "none";
-  }
   slideIndex++;
   if (slideIndex > slide.length) {
     slideIndex = 1;
   }
   slide[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000);
 }
