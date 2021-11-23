05577fc964549562a76c142d0fee6ad791ff70ca
AlessandroGuelpa <67553276+AlessandroGuelpa@users.noreply.github.com>
2021-11-23 11:53:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -47,10 +47,10 @@ for (var i = 0; i < 10; i++) {
   // se il valore nella posizione "i" nell'array è minore di 10
   if (array[i] < 10) {
     // rendi visibile l'elemento "new"
-    document.getElementsByClassName("new")[i].setAttribute("style","visibility = visible");
+    document.getElementsById("new")[i].setAttribute("style","visibility = visible");
     // altrimenti se il valore è maggiore/uguale a 10
   } else {
     // rendi visibile l'elemento "sold"
-    document.getElementsByClassName("sold")[i].setAttribute("style","visibility = visible");
+    document.getElementsById("sold")[i].setAttribute("style","visibility = visible");
   }
 }
