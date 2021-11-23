ee84b8ba82fa8287c268571f37083745b41b25a3
AlessandroGuelpa <Alexander.guelpa@gmail.com>
2021-11-23 12:20:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ for (var i = 0; i < 10; i++) {
   // se il valore nella posizione "i" nell'array è minore di 10
   if (array[i] < 10) {
     // rendi visibile l'elemento "new"
-    ocument.getElementsByClassName("new")[i].style.visibility = "visible";
+    document.getElementsByClassName("new")[i].style.visibility = "visible";
     // altrimenti se il valore è maggiore/uguale a 10
   } else {
     // rendi visibile l'elemento "sold"
