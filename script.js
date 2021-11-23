426341a0d9567f0bf700183db337dc1349cf9c06
AlessandroGuelpa <Alexander.guelpa@gmail.com>
2021-11-23 12:19:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ prev.addEventListener("click", () => {
 let width = carousel.offsetWidth;
 // quando la finestra viene deformata, salva la larghezza nella variabile
 window.addEventListener("resize", () => (width = carousel.offsetWidth));
-// creazione array 
+// creazione array
 var array = [];
 // ciclo da 0 a 9
 for (var i = 0; i < 10; i++) {
@@ -47,10 +47,10 @@ for (var i = 0; i < 10; i++) {
   // se il valore nella posizione "i" nell'array è minore di 10
   if (array[i] < 10) {
     // rendi visibile l'elemento "new"
-    document.getElementByClassName("new")[i].setAttribute("style","visibility = visible");
+    ocument.getElementsByClassName("new")[i].style.visibility = "visible";
     // altrimenti se il valore è maggiore/uguale a 10
   } else {
     // rendi visibile l'elemento "sold"
-    document.getElementsByClassName("sold")[i].style.visible="visible";
+    document.getElementsByClassName("sold")[i].style.visibility = "visible";
   }
 }
