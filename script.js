33302c4b806bfbd51eb64e84857365095ee722f3
AlessandroGuelpa <Alexander.guelpa@gmail.com>
2021-11-29 15:30:21 +0100

    sum

--- script.js
+++ script.js
@@ -2,21 +2,13 @@
 var array = [];
 for (var i = 0; i <= 9; i++) {
   // spingi nell'array il valore della classe "item"
-  var p = document.querySelectorAll(".item")[i].getAttribute("value");
+  var p = document.querySelectorAll(".provare")[i].getAttribute("value");
   array.push(p);
   // se il valore nella posizione "i" nell'array è minore di 10
   if (array[i] < 10) {
     // rendi visibile l'elemento "new"
-    const nuovo = document.createElement("div");
-    const content = document.createTextNode("New!");
-    nuovo.appendChild(content);
-    nuovo.style.textAlign = "left";
     // altrimenti se il valore è maggiore/uguale a 10
   } else {
     // rendi visibile l'elemento "sold"
-    const sold = document.createElement("div");
-    const content = document.createTextNode("Sold!");
-    sold.appendChild(content);
-    sold.style.textAlign = "right";
   }
 }
