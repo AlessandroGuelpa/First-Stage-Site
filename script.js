c41208dc02122b25e9bb53f78b3fa6b0149a8771
AlessandroGuelpa <Alexander.guelpa@gmail.com>
2021-11-29 14:20:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var array = [];
 // ciclo da 0 a 9
-for (var i = 0; i < 9; i++) {
+var array = [];
+for (var i = 0; i <= 9; i++) {
   // spingi nell'array il valore della classe "item"
-  let p = document.getElementsByClassName("item")[i].getAttribute("value");
+  var p = document.querySelectorAll(".item")[i].getAttribute("value");
   array.push(p);
   // se il valore nella posizione "i" nell'array è minore di 10
   if (array[i] < 10) {
     // rendi visibile l'elemento "new"
     const nuovo = document.createElement("div");
-    const content = document.createTextNode("New!")
+    const content = document.createTextNode("New!");
     nuovo.appendChild(content);
-    nuovo.style.textAlign = "left"
+    nuovo.style.textAlign = "left";
     // altrimenti se il valore è maggiore/uguale a 10
   } else {
     // rendi visibile l'elemento "sold"
     const sold = document.createElement("div");
-    const content = document.createTextNode("Sold!")
+    const content = document.createTextNode("Sold!");
     sold.appendChild(content);
-    sold.style.textAlign = "right"
+    sold.style.textAlign = "right";
   }
 }
