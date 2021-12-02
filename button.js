75fa4234ff8959556ec235a3f4dbd8bb2d052070
AlessandroGuelpa <Alexander.guelpa@gmail.com>
2021-12-02 10:11:20 +0100

    updates

--- button.js
+++ button.js
@@ -1,18 +1,24 @@
-//quando la finestra cambia la propria y, viene compilata la funzione scroll
+//quando la finestra cambia la propria y, viene chiamata la funzione scroll
 window.onscroll = function () {
   scroll();
 };
+// salvo in una variabile il bottone
 var btn = document.querySelector("#btn");
 function scroll() {
+  // se lo scroll dall'alto è maggiore di 300
   if (
     document.body.scrollTop > 300 ||
     document.documentElement.scrollTop > 300
   ) {
+    // rendi visibile il bottone
     btn.style.visibility = "visible";
+    // se lo scroll è minore di 300,  nascondi il bottone
   } else {
     btn.style.visibility = "hidden";
   }
 }
+// funzione quando clicchi il bottone
 function return_to_top() {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
+  // lo scrollTop torna all'inizio della pagina, inpiegandoci 2 secondi (2000 ms)
+  $("html, body").animate({ scrollTop: 0 }, 2000);
 }
--- nav.js
+++ nav.js
@@ -1,4 +1,7 @@
+// quando clicchi sul burger
 function funzione() {
+  // salva in una variabile la classe work
   let nav = document.querySelector(".work");
+  // aggiungi alla classe ".work" la classe ".open" che aprirà il menù
   nav.classList.toggle("open");
 }
